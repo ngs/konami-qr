a8aada076101e03e3fa3974e93dee47609c6bc98
Atsushi Nagase <a@ngs.io>
2022-03-11 14:40:51 +0900

    Initial Commit

---
+++ KonamiQR/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KonamiQR
+//
+//  Created by Atsushi Nagase on 2022/03/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KonamiQR/KonamiQRApp.swift
@@ -0,0 +1,17 @@
+//
+//  KonamiQRApp.swift
+//  KonamiQR
+//
+//  Created by Atsushi Nagase on 2022/03/11.
+//
+
+import SwiftUI
+
+@main
+struct KonamiQRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
