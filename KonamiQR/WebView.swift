13d6ca7a6702fae5a8dc2ca218de50f14995b14e
Atsushi Nagase <a@ngs.io>
2022-06-28 17:15:04 +0900

    Update WebView component to support Xcode 14

--- KonamiQR/WebView.swift
+++ KonamiQR/WebView.swift
@@ -16,18 +16,25 @@ struct WebView: UIViewRepresentable {
     var username: String
     var password: String
     var autoNavigate: Bool
-    let webViewDelegate = WebViewDelegate()
     var webView: WKWebView?
+
+    init(url: KonamiURL, username: String, password: String, autoNavigate: Bool) {
+        self.url = url
+        self.username = username
+        self.password = password
+        self.autoNavigate = autoNavigate
+    }
 
     func makeUIView(context: Context) -> WKWebView {
         return WKWebView()
     }
 
     func updateUIView(_ webView: WKWebView, context: Context) {
+        let webViewDelegate = webView.navigationDelegate as? WebViewDelegate ?? WebViewDelegate()
+        webView.navigationDelegate = webViewDelegate
         webViewDelegate.username = username
         webViewDelegate.password = password
         webViewDelegate.autoNavigate = autoNavigate
-        webView.navigationDelegate = webViewDelegate
         let request = URLRequest(url: url.url)
         webView.load(request)
     }
